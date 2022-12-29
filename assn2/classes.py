ceb019cf499ebf6082c432e21f5bf801fdcd78c3
Andrew Pauls <91347926+wallaby55@users.noreply.github.com>
2022-12-29 12:43:32 -0500

    Add files via upload
    more work on assn2
    should be done by today
    commenting is minimal
    rough work primarily throughout this work

--- assn2/classes.py
+++ assn2/classes.py
@@ -32,19 +32,18 @@ class Drug:
     def displayDrug(self):
         print("Generic Name: ", self.genericName, " dbID: ", self.drugBankID, " score: ", self.score)
 
-a1 = Drug("", "donkey","","","","") 
 
 # class3, DrugBank
 class DrugBank:
 
+
+    
         ## constructor?
     def __init__(self, msg):
         print(msg)
-        
-
-        
-        
-    
+        self.drugs = list(range(1933))
+        self.a1 = Drug("","","","","","")
+        self.root = BinaryNode(None, None, None)
         
         
     ## methods
@@ -54,14 +53,8 @@ class DrugBank:
     # 2. depth1(string dbID) --- prints int for depth of node being searched for
 
     # 3. readData()
-
-
     def readData(self):
-        drugs = list(range(1933))
-       
         
-
-       
         # 1. get text file
         import csv
         with open("dockedApproved.tab", "r") as file:
@@ -69,22 +62,78 @@ class DrugBank:
             i = 0
             print('value of i', i)
             global a1
+            global drugs
             for line in line_reader:
         
-                a1 = (line[0], line[1], line[2], line[3], line[4], line[5])
-                drugs[i] = a1
+                self.a1 = Drug(line[0], line[1], line[2], line[3], line[4], line[5])
+                self.drugs[i] = self.a1
                 i = i + 1
         file.close()
-        for i in range(0, 1933):
-            d = drugs[i]
-            print(i, "th drug. ", d[0], " is gen name.")
+    
             
   
-    
     # 4. inOrderTraverse()
-
+    def inOrderTraverse(self,subT):
+        if( subT is None ):
+            return
+        else:
+            self.inOrderTraverse(subT.leftC)
+            print(subT.Data.genericName, ", ID:", subT.Data.drugBankID)
+            self.inOrderTraverse(subT.rightC)
+                        
     # 5. insert(drug d)
+    def insertDrug(self,drug2insert):
+        current = self.root
 
-    # 6. boolean search(String dbID)
+        ## check if we have an empty tree
+        if (current.Data == None):
+            self.root.Data = drug2insert
 
+        ## else, tree not empty
+        else:
+            while (True):
+                if(drug2insert.drugBankID < current.Data.drugBankID):
+                    if (current.leftC == None):
+                        current.leftC = BinaryNode(drug2insert, None,None)
+                        return
+                    else:
+                        current = current.leftC
+
+                else:
+                    if(current.rightC == None):
+                        current.rightC = BinaryNode(drug2insert, None,None)
+                        return
+                    else:
+                        current = current.rightC
+
+    def factoryInsert(self):
+        # for loop, for each line in drugs, insert into the BST
+        for i in range(1, 1933):
+            self.insertDrug(self.drugs[i])
+
+    # search(root, key)
+    
+    def search(self, dbID):
+        current = self.root
+        print(current.Data.drugBankID)
+        
+        while(current.Data.drugBankID != dbID):
+            
+
+            if(int(dbID[2:6])  <  int(current.Data.drugBankID[2:6])):
+                current = current.leftC
+            else:
+                current = current.rightC
+
+            ## print out the current value
+            print(current.Data.drugBankID, " is currents dbID")
+            
+            if(current is None):
+                print("not found")
+                return None
+        return current
+
+    
+        
+ 
     
--- assn2/main.py
+++ assn2/main.py
@@ -9,6 +9,7 @@
 from classes import DrugBank, Drug, BinaryNode
 
 
+
 ## instantiate what needs to be instantiated
 
 db = DrugBank("hello")
@@ -16,3 +17,25 @@ db = DrugBank("hello")
 ## run what needs to be ran
 db.readData()
 
+
+## now lets add every single drug to the thing
+db.factoryInsert()
+
+
+## search for: DB14646
+print("searching for: DB09394")
+drugFound = db.search('DB09394')
+print(drugFound.Data.drugBankID, ' was found')
+
+print("searching for: DB08905")
+drugFound2 = db.search('DB08905')
+print(drugFound2.Data.drugBankID, ' was found')
+
+print("searching for: DB13628")
+drugFound3 = db.search('DB13628')
+print(drugFound3.Data.drugBankID, ' was found')
+
+
+print("searching for: DB00765")
+drugFound4 = db.search('DB00765')
+print(drugFound4.Data.drugBankID, ' was found')
